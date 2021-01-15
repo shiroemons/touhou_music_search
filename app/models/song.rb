432d386eae8b06c526d9a85ce396e9c0171f6993
shiroemons <shiroemons@gmail.com>
2021-01-15 19:24:22 +0900

    Add scope lyrics

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,4 +5,6 @@ class Song < ApplicationRecord
   scope :not_touhou, -> { where(is_touhou: false) }
   scope :streamable, -> { where(is_streamable: true)}
   scope :not_streamable, -> { where(is_streamable: false)}
+  scope :has_lyrics, -> { where(has_lyrics: true) }
+  scope :has_not_lyrics, -> { where(has_lyrics: false) }
 end
