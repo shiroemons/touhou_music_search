4271753589d9773a265a3ba0a019a11598082fe6
shiroemons <shiroemons@gmail.com>
2021-01-18 01:22:13 +0900

    Add a scope to narrow down the distribution of 東方同人音楽流通

--- app/models/discography.rb
+++ app/models/discography.rb
@@ -1,6 +1,7 @@
 class Discography < ApplicationRecord
   has_many :songs, -> { order(Arel.sql('songs.track_number ASC')) }, dependent: :destroy
 
+  scope :touhou_doujin, -> { where(record_label: '東方同人音楽流通') }
   scope :touhou, -> { where(is_touhou: true) }
   scope :not_touhou, -> { where(is_touhou: false) }
   scope :various_artists, -> { where(artist_name: "Various Artists") }
