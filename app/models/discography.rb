62fa48d8efb7bc367b09c8f19dba5a01f290fafa
shiroemons <shiroemons@gmail.com>
2021-01-24 17:40:30 +0900

    Add scope

--- app/models/discography.rb
+++ app/models/discography.rb
@@ -1,6 +1,10 @@
 class Discography < ApplicationRecord
   has_many :songs, -> { order(Arel.sql('songs.track_number ASC')) }, dependent: :destroy
 
+  scope :includes_songs, -> { includes(:songs) }
+  scope :streamable, -> { includes_songs.where(songs: { is_streamable: true }) }
+  scope :not_streamable, -> { includes_songs.where(songs: { is_streamable: false }) }
+
   scope :touhou_doujin, -> { where(record_label: '東方同人音楽流通') }
   scope :touhou, -> { where(is_touhou: true) }
   scope :not_touhou, -> { where(is_touhou: false) }
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,9 @@
 class Song < ApplicationRecord
   belongs_to :discography
 
+  scope :includes_discography, -> { includes(:discography) }
+  scope :touhou_doujin, -> { includes_discography.where(discographies: { record_label: '東方同人音楽流通' }) }
+
   scope :touhou, -> { where(is_touhou: true) }
   scope :not_touhou, -> { where(is_touhou: false) }
   scope :streamable, -> { where(is_streamable: true)}
