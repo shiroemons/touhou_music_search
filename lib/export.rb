133f25f714fa6d8cdf9abaa59f5ad73d43a39d28
shiroemons <shiroemons@gmail.com>
2021-01-24 17:45:51 +0900

    Update export

--- lib/export.rb
+++ lib/export.rb
@@ -3,6 +3,12 @@ File.open("tmp/touhou_music.tsv", "w") do |f|
   f.puts("collection_name\ttrack_name\tartist_name\ttrack_number\trelease_date\tcollection_view_url\ttrack_view_url")
   Discography.includes(:songs).touhou_doujin.order(:release_date).each do |d|
     collection_view_url = d.collection_view_url
+    id = d.apple_collection_id
+    track_count = d.track_count
+    song_count = d.songs.count
+    if track_count != song_count
+      puts "#{id}\t#{d.apple_music_collection_name}\t#{track_count}\t#{song_count}"
+    end
     d.songs.each do |song|
       collection_name = song.collection_name
       track_name = song.track_name
@@ -21,7 +27,7 @@ end
 
 File.open("tmp/touhou_music_not_apple_music.tsv", "w") do |f|
   f.puts("collection_name\ttrack_name\tartist_name\tcomposer_name\ttrack_number\trelease_date\tcollection_view_url\ttrack_view_url")
-  Discography.includes(:songs).touhou.order(:release_date).each do |d|
+  Discography.includes(:songs).touhou_doujin.touhou.order(:release_date).each do |d|
     collection_view_url = d.collection_view_url
     d.songs.each do |song|
       collection_name = song.collection_name
@@ -31,7 +37,7 @@ File.open("tmp/touhou_music_not_apple_music.tsv", "w") do |f|
       track_number = song.track_number
       release_date = song.release_date
       track_view_url = song.track_view_url
-      unless song.is_streamable
+      if song.is_touhou && !song.is_streamable
         f.puts("#{collection_name}\t#{track_name}\t#{artist_name}\t#{composer_name}\t#{track_number}\t#{release_date}\t#{collection_view_url}\t#{track_view_url}")
       end
     end
@@ -40,13 +46,13 @@ end
 
 File.open("tmp/touhou_music_has_lyrics.tsv", "w") do |f|
   f.puts("collection_name\ttrack_name\tartist_name\ttrack_view_url")
-  Discography.includes(:songs).touhou.order(:release_date).each do |d|
+  Discography.includes(:songs).touhou_doujin.touhou.order(:artist_name).each do |d|
     d.songs.each do |song|
       collection_name = song.collection_name
       track_name = song.track_name
       artist_name = song.artist_name
       track_view_url = song.track_view_url
-      if song.is_streamable && song.has_lyrics
+      if song.is_touhou && song.is_streamable && song.has_lyrics
         f.puts("#{collection_name}\t#{track_name}\t#{artist_name}\t#{track_view_url}")
       end
     end
@@ -55,7 +61,7 @@ end
 
 File.open("tmp/touhou_music_albums.tsv", "w") do |f|
   f.puts("artist_name\tcollection_name\tcollection_view_url")
-  Discography.includes(:songs).touhou.order(:artist_name).order(:apple_music_collection_name).each do |d|
+  Discography.includes(:songs).touhou_doujin.touhou.order(:artist_name).order(:apple_music_collection_name).each do |d|
     artist_name = d.artist_name
     collection_name = d.apple_music_collection_name
     collection_view_url = d.collection_view_url.gsub('?uo=4', '')
@@ -65,7 +71,7 @@ end
 
 File.open("tmp/touhou_music_albums_with_songs.tsv", "w") do |f|
   f.puts("apple_collection_id\tcollection_name")
-  Discography.includes(:songs).touhou.order(:apple_collection_id).each do |d|
+  Discography.includes(:songs).touhou_doujin.touhou.order(:apple_collection_id).each do |d|
     collection_name = d.collection_name
     d.songs.each do |song|
       track_name = song.track_name
@@ -78,7 +84,7 @@ File.open("tmp/touhou_music_albums_with_songs.tsv", "w") do |f|
 end
 
 apple_music_songs = []
-Song.touhou.streamable.find_each do |song|
+Song.touhou_doujin.touhou.streamable.find_each do |song|
   track_name = song.track_name
   collection_name = song.collection_name
   track_view_url = song.track_view_url.gsub('&uo=4','')
@@ -90,11 +96,13 @@ File.open("tmp/touhou_music_song_apple.json", "w") do |f|
 end
 
 youtube_music_songs = []
-Song.touhou.youtube_music.order(:collection_name).find_each do |song|
+Song.touhou_doujin.touhou.youtube_music.order(:collection_name).find_each do |song|
   track_name = song.youtube_track_name
   collection_name = song.youtube_collection_name
   track_view_url = song.youtube_track_view_url
-  youtube_music_songs.push({ title: track_name, collection_name: collection_name, url: track_view_url})
+  if track_name.present? && track_name != "不明"
+    youtube_music_songs.push({ title: track_name, collection_name: collection_name, url: track_view_url})
+  end
 end
 
 File.open("tmp/touhou_music_song_youtube.json", "w") do |f|
