82580717a7019c79fe4366e486209cfcae071113
shiroemons <shiroemons@gmail.com>
2021-04-04 16:56:41 +0900

    Add search for original song title

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -12,10 +12,23 @@ module Types
       argument :limit, Integer, required: false
     end
     def songs(page: nil, limit: nil)
-      ::Song.includes(original_songs: :original)
+      Song.includes(original_songs: :original)
             .touhou_doujin
             .order(release_date: :desc, track_number: :asc)
             .page(page).per(limit)
+    end
+
+    field :songs, Types::SongType.collection_type, null: true do
+      argument :page, Integer, required: false
+      argument :limit, Integer, required: false
+      argument :original_song_title, String, required: true
+    end
+    def songs(page: nil, limit: nil, original_song_title: nil)
+      Song.includes(original_songs: :original)
+          .touhou_doujin
+          .order(release_date: :desc, track_number: :asc)
+          .where(original_songs: { title: original_song_title })
+          .page(page).per(limit)
     end
   end
 end
