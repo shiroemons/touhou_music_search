21c795e22b2486d9ccb54e50475732792f0e3267
shiroemons <shiroemons@gmail.com>
2021-04-08 01:05:36 +0900

    Fix Sprockets::FileNotFound: couldn't find file 'graphiql/rails/application.css'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.assets.precompile += %w[graphiql/rails/application.js graphiql/rails/application.css]
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
