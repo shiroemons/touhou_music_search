b6cff969190257c1bdcb2a3438d31d91dbc53873
shiroemons <shiroemons@gmail.com>
2021-04-10 01:12:10 +0900

    Add discographies query_type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,6 +9,14 @@ module Types
       Original.original_song_non_duplicated.order(:code)
     end
 
+    field :discographies, Types::DiscographyType.collection_type, null: true do
+      argument :page, Integer, required: false
+      argument :limit, Integer, required: false
+    end
+    def discographies(page: nil, limit: nil)
+      Discography.includes_songs.touhou_doujin.touhou.order(release_date: :desc).page(page).per(limit)
+    end
+
     field :songs, Types::SongType.collection_type, null: true do
       argument :page, Integer, required: false
       argument :limit, Integer, required: false
