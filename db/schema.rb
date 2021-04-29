2db1843073e9a8ecc255ad6a738dcfae68345a9b
shiroemons <shiroemons@gmail.com>
2021-04-29 11:16:15 +0900

    Add spotify columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_004953) do
+ActiveRecord::Schema.define(version: 2021_04_29_021406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2021_04_09_004953) do
     t.string "youtube_collection_name", default: "", null: false
     t.string "youtube_collection_view_url", default: "", null: false
     t.datetime "last_fetched_at", precision: 6
+    t.string "spotify_collection_name", default: "", null: false
+    t.string "spotify_collection_view_url", default: "", null: false
     t.index ["apple_artist_id"], name: "index_discographies_on_apple_artist_id"
     t.index ["apple_collection_id"], name: "index_discographies_on_apple_collection_id", unique: true
   end
@@ -133,6 +135,10 @@ ActiveRecord::Schema.define(version: 2021_04_09_004953) do
     t.string "youtube_track_name", default: "", null: false
     t.string "youtube_collection_view_url", default: "", null: false
     t.string "youtube_track_view_url", default: "", null: false
+    t.string "spotify_collection_name", default: "", null: false
+    t.string "spotify_track_name", default: "", null: false
+    t.string "spotify_collection_view_url", default: "", null: false
+    t.string "spotify_track_view_url", default: "", null: false
     t.index ["apple_artist_id"], name: "index_songs_on_apple_artist_id"
     t.index ["apple_collection_id"], name: "index_songs_on_apple_collection_id"
     t.index ["apple_track_id"], name: "index_songs_on_apple_track_id", unique: true
